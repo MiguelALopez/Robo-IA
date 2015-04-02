b91937d7e8de26b38b92fb64c632e3c530a5fbd3
miguel <miguel.angel.lopez@correounivalle.edu.co>
2015-04-02 12:59:46 -0500

    First Commit, add MenuPrincipal

---
+++ src/Interfaz/MenuPrincipal.java
@@ -0,0 +1,15 @@
+/**
+ * ********************************************
+ * Autor: Miguel Angel Lopez Fernandez
+ * Correo: miguel.angel.lopez@correounivalle.edu.co
+ * Código: 1326691
+ * Fecha: 02-abr-2015
+ * Nombre del Archivo: MenuPrincipal.java
+ * Plan: Ingeniería de Sistemas - 3743
+ * Institución Educativa: Universidad del Valle
+ * *********************************************
+ */
+package Interfaz;
+
+public class MenuPrincipal {
+}
